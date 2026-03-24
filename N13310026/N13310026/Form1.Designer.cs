1727f61ec3ffb162cf4ed4a79e447f3b85e0701a
peng1117 <2003jim921117@gmail.com>
2026-03-24 21:08:26 +0800

    20260324

--- N13310026/N13310026/Form1.Designer.cs
+++ N13310026/N13310026/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_ClickMe = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_ClickMe
+            // 
+            this.btn_ClickMe.BackColor = System.Drawing.Color.Pink;
+            this.btn_ClickMe.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_ClickMe.Location = new System.Drawing.Point(65, 114);
+            this.btn_ClickMe.Name = "btn_ClickMe";
+            this.btn_ClickMe.Size = new System.Drawing.Size(186, 96);
+            this.btn_ClickMe.TabIndex = 0;
+            this.btn_ClickMe.Text = "請按一下";
+            this.btn_ClickMe.UseVisualStyleBackColor = false;
+            this.btn_ClickMe.Click += new System.EventHandler(this.btn_ClickMe_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 61);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N13310026_康";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ClickMe);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_ClickMe;
+        private System.Windows.Forms.Label label1;
     }
 }
 
